b0a11a8604f488441cb6d4465495974cb491bedb
Peter Jiang <rcffc@users.noreply.github.com>
2020-12-17 11:49:19 +0100

    Changed up some stuff
    Tried ICP with Generalized ICP Repo, but transformation too small.

--- demo.py
+++ demo.py
@@ -6,20 +6,58 @@ import time
 import cv2
 import numpy as np
 import pyk4a
-from pyk4a import depth_image_to_point_cloud, Config, PyK4A
+from pyk4a import Config, PyK4A
 import fusion
-import icp.basicICP as icp
-
+from numba import jit
+#import icp.basicICP as icp
+import Generalized_ICP.Generalized_ICP
 
 def configureCamera():
   k4a = PyK4A(
     Config(
       color_resolution=pyk4a.ColorResolution.RES_720P,
       depth_mode=pyk4a.DepthMode.NFOV_UNBINNED,
-      synchronized_images_only=False
+      synchronized_images_only=True
     )
   )
   return k4a
+
+
+def depth_image_to_point_cloud(map, h, w, K):
+  points = np.ndarray((h * w, 3))
+  return depth_image_to_point_cloud_numba(map, h, w, K, points)
+
+
+@jit(nopython=True)
+def depth_image_to_point_cloud_numba(map, h, w, K, points):
+  for u, row in enumerate(map):
+    for v, depth in enumerate(row):
+
+      u_ndc = u / w
+      v_ndc = v / h
+
+      u_screen = u_ndc * w - w / 2
+      v_screen = v_ndc * h - h / 2
+
+      fx = K[0][0]
+      fy = K[1][1]
+      fxinv = 1.0 / fx
+      fyinv = 1.0 / fy
+
+      cx = K[0][2]
+      cy = K[1][2]
+
+      points[u * w + v] = [(u_screen - cx) * fxinv * depth, (v_screen - cy) * fyinv * depth, -depth]
+  return points
+
+
+def icp(curr, prev, method):
+  R, T, rms = Generalized_ICP.Generalized_ICP.ICP(curr, prev, method)
+  print(rms)
+  pose = np.column_stack((R, T))
+  np.vstack((pose, np.array([0, 0, 0, 1])))
+  return pose
+
 
 if __name__ == "__main__":
   # ======================================================================================================== #
@@ -29,21 +67,21 @@ if __name__ == "__main__":
   # ======================================================================================================== #
   print("Estimating voxel volume bounds...")
   n_imgs = 1000
-  cam_intr = np.loadtxt("data/camera-intrinsics.txt", delimiter=' ')
+  cam_intr = np.loadtxt("data_original/camera-intrinsics.txt", delimiter=' ')
   vol_bnds = np.zeros((3,2))
   for i in range(n_imgs):
     # Read depth image and camera pose
-    depth_im = cv2.imread("data/frame-%06d.depth.png"%(i),-1).astype(float)
+    depth_im = cv2.imread("data_original/frame-%06d.depth.png"%(i),-1).astype(float)
     depth_im /= 1000.  # depth is saved in 16-bit PNG in millimeters
     depth_im[depth_im == 65.535] = 0  # set invalid depth to 0 (specific to 7-scenes dataset)
-    cam_pose = np.loadtxt("data/frame-%06d.pose.txt"%(i))  # 4x4 rigid transformation matrix
+    cam_pose = np.loadtxt("data_original/frame-%06d.pose.txt"%(i))  # 4x4 rigid transformation matrix
 
     # Compute camera view frustum and extend convex hull
     view_frust_pts = fusion.get_view_frustum(depth_im, cam_intr, cam_pose)
     vol_bnds[:,0] = np.minimum(vol_bnds[:,0], np.amin(view_frust_pts, axis=1))
     vol_bnds[:,1] = np.maximum(vol_bnds[:,1], np.amax(view_frust_pts, axis=1))
   # ======================================================================================================== #
-
+  print(vol_bnds)
   # ======================================================================================================== #
   # Integrate
   # ======================================================================================================== #
@@ -59,15 +97,14 @@ if __name__ == "__main__":
   depth_im = cv2.imread("data/frame-%06d.depth.png"%(0),-1).astype(float)
   depth_im /= 1000.
   depth_im[depth_im == 65.535] = 0
-  cam_pose = np.loadtxt("data/frame-%06d.pose.txt" % (0))
+  cam_pose = np.loadtxt("data_original/frame-%06d.pose.txt" % (0))
 
-  k4a = configureCamera()
-  k4a.start()
-  k4a.whitebalance = 4510
-
-  curr = depth_image_to_point_cloud(depth_im, k4a.calibration, True)
   height = depth_im.shape[0]
   width = depth_im.shape[1]
+  camera_intrinsics = np.loadtxt("data/camera-intrinsics.txt")
+  curr = Generalized_ICP.Generalized_ICP.Point_cloud()
+  curr.init_from_points(depth_image_to_point_cloud(depth_im, height, width, camera_intrinsics))
+
 
   for i in range(n_imgs):
     print("Fusing frame %d/%d"%(i+1, n_imgs))
@@ -79,11 +116,13 @@ if __name__ == "__main__":
     depth_im[depth_im == 65.535] = 0
 
     prev = curr
-    curr = depth_image_to_point_cloud(depth_im)
-    cam_pose = icp.icp_point_to_plane(prev, curr)
-    with open('data_pose/frame-' + "{:02d}".format(i) + '.pose.txt', 'w') as pose_file:
+    curr.init_from_points(depth_image_to_point_cloud(depth_im, height, width, camera_intrinsics))
+    cam_transformation = icp(curr, prev, "point2plane")
+    cam_pose = cam_transformation @ cam_pose
+
+    with open("data/frame-%06d.pose.txt" % (i), 'w') as pose_file:
       for row in cam_pose:
-        pose_file.write(row[0] + ' ' + row[1] + ' ' + row[2] + ' ' + row[3] + '\n')
+        pose_file.write(str(row[0]) + ' ' + str(row[1]) + ' ' + str(row[2]) + ' ' + str(row[3]) + '\n')
 
     # Integrate observation into voxel volume (assume color aligned with depth)
     tsdf_vol.integrate(color_image, depth_im, cam_intr, cam_pose, obs_weight=1.)
